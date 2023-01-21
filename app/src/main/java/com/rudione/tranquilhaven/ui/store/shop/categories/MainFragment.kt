a49e68ecd8d1ee2ee005811984835c2583a01cbf
Rudione <denisrudik01@gmail.com>
2023-01-21 23:44:54 +0200

    Features: add main category, add home screen

--- app/src/main/java/com/rudione/tranquilhaven/ui/store/shop/categories/MainFragment.kt
+++ app/src/main/java/com/rudione/tranquilhaven/ui/store/shop/categories/MainFragment.kt
@@ -3,5 +3,4 @@ package com.rudione.tranquilhaven.ui.store.shop.categories
 import androidx.fragment.app.Fragment
 import com.rudione.tranquilhaven.R
 
-class MainFragment(): Fragment(R.layout.fragment_main_category) {
-}
+class MainFragment: Fragment(R.layout.fragment_main_category)
