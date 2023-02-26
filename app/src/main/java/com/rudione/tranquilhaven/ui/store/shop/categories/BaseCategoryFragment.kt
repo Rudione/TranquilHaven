2952b78555109546815a4f3382331c8564e01353
Rudione <denisrudik01@gmail.com>
2023-02-26 16:51:14 +0200

    Features: fix bugs, add BaseCategory

--- app/src/main/java/com/rudione/tranquilhaven/ui/store/shop/categories/BaseCategoryFragment.kt
+++ app/src/main/java/com/rudione/tranquilhaven/ui/store/shop/categories/BaseCategoryFragment.kt
@@ -5,6 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.rudione.tranquilhaven.R
+import com.rudione.tranquilhaven.databinding.FragmentBaseCategoryBinding
+import com.rudione.tranquilhaven.ui.store.shop.adapters.BestProductAdapter
 
-abstract class BaseCategoryFragment : Fragment(R.layout.fragment_base_category)
+open class BaseCategoryFragment : Fragment(R.layout.fragment_base_category) {
+    private lateinit var binding: FragmentBaseCategoryBinding
+    private lateinit var offerAdapter: BestProductAdapter
+    private lateinit var bestProductAdapter: BestProductAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBaseCategoryBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupOfferRv()
+        setupBestProductsRv()
+    }
+
+    private fun setupOfferRv() {
+        offerAdapter = BestProductAdapter()
+        binding.rvBestProducts.apply {
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = offerAdapter
+        }
+    }
+
+    private fun setupBestProductsRv() {
+        bestProductAdapter = BestProductAdapter()
+        binding.rvBestProducts.apply {
+            layoutManager = GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+            adapter = bestProductAdapter
+        }
+    }
+}
